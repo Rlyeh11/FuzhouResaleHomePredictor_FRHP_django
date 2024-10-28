f1aa5aa8dc94c6686b91e28062e164c143f1b237
Rlyeh11 <Rlyeh11@users.noreply.github.com>
2024-10-28 13:21:56 +0800

    final_version: 毕业版本

--- FRHP/api/views.py
+++ FRHP/api/views.py
@@ -17,6 +17,7 @@ print(os.path.abspath(__file__))
 model_date = '2024_03_19 13_53_46'
 kmeans = joblib.load(f'api/model/kmeans_{model_date}_GBRT.model')
 GBRT= joblib.load(f'api/model/GBRT/GBRT_{model_date}.model')
+RF= joblib.load(f'api/model/RF/RF_{model_date}.model')
 encoder = joblib.load('api/model/encoder.pkl')
 
 # Create your views here.
@@ -40,7 +41,7 @@ def predict_houseprice(request):
                 # print(features_df.columns)
                 features_df.columns = ['行政区', '房屋类型', '房屋装修', '房屋结构', '房屋朝向', '房屋面积', '楼层数', '楼层位置',
                                                    '经度_wgs', '纬度_wgs']
-                # tags = ['中餐厅', '小吃快餐店', '酒吧', '购物中心', '百货商场', '便利店', '市场', '洗衣店', '宠物服务', '公园', '景点', '文物古迹', '农家院', '电影院', 'ktv', '休闲广场', '体育场馆', '健身中心', '中学', '小学', '幼儿园', '综合医院', '诊所', '药店', '急救中心', '医疗保健', '地铁站', '公交车站', '停车场', '银行', '信用社', '公司', '园区']
+                tags = ['中餐厅', '小吃快餐店', '酒吧', '购物中心', '百货商场', '便利店', '市场', '洗衣店', '宠物服务', '公园', '景点', '文物古迹', '农家院', '电影院', 'ktv', '休闲广场', '体育场馆', '健身中心', '中学', '小学', '幼儿园', '综合医院', '诊所', '药店', '急救中心', '医疗保健', '地铁站', '公交车站', '停车场', '银行', '信用社', '公司', '园区']
                 KDTrees = joblib.load(r'api/model/KDtrees.pkl')
                 trees = KDTrees['trees']
                 gdfs = KDTrees['gdfs']
@@ -58,15 +59,23 @@ def predict_houseprice(request):
                         if distance <= 1500:
                             col = str(category) + '_1.5km'
                             features_df.loc[index, col] = 1
+                # 数据类型处理
                 module_features_without_position, p = get_X_p(features_df, encoder)
                 clusters = kmeans.predict(p)
-                pred = [GBRT[cluster].predict(module_features_without_position.iloc[i].values.reshape(1, -1))[0] for i, cluster in
-                        enumerate(clusters)]
+                print(clusters)
+                GBRT_pred = [GBRT[cluster].predict(module_features_without_position.iloc[i].values.reshape(1, -1))[0]
+                             for i, cluster in enumerate(clusters)]
+                RF_pred = [RF[cluster].predict(module_features_without_position.iloc[i].values.reshape(1, -1))[0]
+                           for i, cluster in enumerate(clusters)]
+                print('::features::')
+                for key, val in features.items():
+                    print(key, ':', val)
+                print('::::::::::::')
 
             except KeyError as e:
                 print(e)
                 return JsonResponse({'status': '200', 'msg': 'data not valid'})
-            return JsonResponse({'status': '200', 'msg': 'request post successfully', 'data': pred[0]})
+            return JsonResponse({'status': '200', 'msg': 'request post successfully', 'data': {'GBRT':round(GBRT_pred[0], 2), 'RF':round(RF_pred[0], 2)}})
         else:
             return JsonResponse({'status': '400', 'msg': 'data is missing in the request'})
 
--- FRHP/introduce/views.py
+++ FRHP/introduce/views.py
@@ -1,13 +1,25 @@
+import re
+
 from django.shortcuts import HttpResponse, render
 from django.views.decorators.csrf import csrf_exempt
 
 
+def check_cookie(request, key):
+    status = request.COOKIES.get(key)
+    return status
+
+def check_background(request):
+    color = check_cookie(request, 'background_color')
+    if color == None:
+        color = '#c0ccda'
+    return color
 
 
 @csrf_exempt
 def introduceIndex(request):
+    color = check_background(request)
     response = render(request, 'index.html')
-    response["Set-Cookie"] = "SameSite=None;Secure"
+    response["Set-Cookie"] = f"SameSite=None;Secure;background_color={color}"
     return response
 
 
